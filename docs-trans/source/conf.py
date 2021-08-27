7ceefc4684e3bc3a466fb2bfb16774f4c90353e8
zhangliyu <1701213742.sz.pku.edu.cn>
2021-08-27 15:58:47 +0800

    readthedocs

--- docs-trans/source/conf.py
+++ docs-trans/source/conf.py
@@ -16,6 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
+from cnvlib import __version__
 
 # -- Project information -----------------------------------------------------
 
